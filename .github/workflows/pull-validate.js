097bc5edc3a2f75a8ba58d2a5ca0f26b6fe3f30f
Geekid812 <geekid812@gmail.com>
2025-04-08 17:40:23 +0200

    add issue comment

--- .github/workflows/pull-validate.js
+++ .github/workflows/pull-validate.js
@@ -1,5 +1,10 @@
 /// GitHub script to validate pull requests
 
-module.exports = ({ github, context }) => {
-    return "Hello World!";
+module.exports = async ({ github, context }) => {
+    await github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: 'Hello World!'
+    });
 }
